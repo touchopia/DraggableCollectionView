3cb1d26f36ed8f78b78366343e410b1c496d01a9
Phil Wright <touchopia@me.com>
2025-04-09 08:12:06 -0600

    Seperating out models

---
+++ DraggableCollectionView/Models/Models.swift
@@ -0,0 +1,50 @@
+//
+//  Models.swift
+//  TinderSwiper
+//
+//  Created by Phil Wright on 4/7/25.
+//
+
+import Foundation
+
+struct Profile {
+    let name: String
+    let imageName: String
+}
+
+let sampleProfiles = [
+    Profile(name: "Alex", imageName: "profile1"),
+    Profile(name: "Sam", imageName: "profile2"),
+    Profile(name: "Jordan", imageName: "profile3"),
+    Profile(name: "Septimus", imageName: "profile4"),
+    Profile(name: "David", imageName: "profile5"),
+    Profile(name: "Luke", imageName: "profile6"),
+    Profile(name: "Jacob", imageName: "profile7"),
+    Profile(name: "Mark", imageName: "profile8"),
+    Profile(name: "John", imageName: "profile9"),
+    Profile(name: "Jerry", imageName: "profile10")
+]
+
+var items = [
+    "Consistent with Words/Actions",
+    "Supportive",
+    "Reciprocity",
+    "Gives and takes feedback",
+    "Repairs after Conflict",
+    "Shows Empathy",
+    "Is Introspective",
+    "Able to compromise",
+    "Communicates",
+    "Active Listening",
+    "Over familiar",
+    "Disconnected",
+    "Deflective",
+    "Not thoughtful",
+    "Isn't self-relective",
+    "Hypercritical",
+    "Hostile",
+    "Controlling",
+    "One-Sided",
+    "Lies"
+]
+
